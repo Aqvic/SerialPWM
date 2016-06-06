94e572c2e64465fded007fe429061718bd7844f2
Aqvic.com <public@aqvic.com>
2016-06-06 08:45:46 +0300

    added leOS2. delay() is evil. Use timers and interrupts, or leOS ;)

--- SerialPWM.ino
+++ SerialPWM.ino
@@ -12,10 +12,16 @@
 *   0x03 (setEEPROM)    0x00-0xFF (address)   0x00-0xFF   VALUE
 *
 *   Status and errors codes see below in constants section
+*   
+*   
+*   Dependencies:
+*   https://github.com/leomil72/leOS2    -   leOS2 by Leonardo Miliani
+*   
 */
 
 #include <Arduino.h>
 #include <EEPROM.h>
+#include "leOS2.h"
 
 // Use static const instead DEFINE. Because it respects scope and is type-safe!
 // Use static const instead MAGIC NUMBERS. Always!
@@ -39,13 +45,78 @@ static const byte PROTO_ERROR_RX_FAULT    = 0xE5;
 static const byte pwmPinsNumber = 4;
 static const byte pwmPins[pwmPinsNumber] = {9, 10 ,11, 5}; // manual initialization
 
-byte setPWMValue(byte data[]){
-  if(data[PARAM]<pwmPinsNumber){
-    analogWrite(pwmPins[data[PARAM]], data[VALUE]);
-    return PROTO_OK;
+static const byte BOARD_LED_PIN = 13;
+boolean pin13State = false;
+
+volatile boolean timeToBlink = false;
+leOS2 myOS;
+
+
+void set30HzPWM();
+void set120HzPWM();
+void set500HzPWM();
+void set4KHzPWM();
+void set31KHzPWM();
+
+void (*pwmFrequency[5])() = {set30HzPWM, set120HzPWM, set500HzPWM, set4KHzPWM, set31KHzPWM};
+
+byte setPWMFrequency(byte data[]);
+byte setPWMValue(byte data[]);
+byte getEEPROM(byte data[]);
+byte setEEPROM(byte data[]);
+
+byte (*proto[PROTO_MAX_COMMANDS])(byte data[]) = {setPWMValue, setPWMFrequency, getEEPROM, setEEPROM};
+
+bool checkXOR(byte data[]);
+
+void blinkBoardLed();
+
+void setup() {
+
+  pinMode(BOARD_LED_PIN, OUTPUT);      // pin 13 - board Led
+  digitalWrite(BOARD_LED_PIN, LOW);
+
+  for(int i=0; i<pwmPinsNumber; i++){
+    pinMode(pwmPins[i], OUTPUT);
+    analogWrite(pwmPins[i], 0);
   }
-  return PROTO_ERROR_WRONG_PARAM;
+    
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+
+  myOS.begin();
+  myOS.addTask([](){timeToBlink = true;}, myOS.convertMs(500));
 }
+
+void loop() {
+
+    if(timeToBlink){
+        blinkBoardLed();
+        timeToBlink = false;
+    }
+}
+
+void serialEvent() {
+
+  byte buf[PROTO_RX_BUFFER_SIZE] = {0xff, 0xff ,0xff, 0xff}; // manual initialization
+        
+  if(Serial.readBytes(buf, PROTO_RX_BUFFER_SIZE) == PROTO_RX_BUFFER_SIZE){
+    if(checkXOR(buf)){
+      if(buf[COMMAND] < PROTO_MAX_COMMANDS){
+        Serial.write((*proto[buf[COMMAND]])(buf));
+      }else{
+        Serial.write(PROTO_ERROR_BAD_COMMAND);
+      }
+    }else{
+      Serial.write(PROTO_ERROR_XOR);
+    }
+  }else{
+    Serial.write(PROTO_ERROR_RX_FAULT);
+  }
+
+  while (Serial.available() > 0){Serial.read();} // clear serial buffer
+}
+
 
 
 void set31KHzPWM(){
@@ -73,9 +144,6 @@ void set30HzPWM(){
     TCCR1B = TCCR1B & B11111000 | B00000101;
 }
 
-void (*pwmFrequency[5])() = {set30HzPWM, set120HzPWM, set500HzPWM, set4KHzPWM, set31KHzPWM};
-
-
 byte setPWMFrequency(byte data[]){
   if(data[PARAM]<5){
     (*pwmFrequency[data[PARAM]])();
@@ -84,6 +152,13 @@ byte setPWMFrequency(byte data[]){
   return PROTO_ERROR_WRONG_PARAM;
 }
 
+byte setPWMValue(byte data[]){
+  if(data[PARAM]<pwmPinsNumber){
+    analogWrite(pwmPins[data[PARAM]], data[VALUE]);
+    return PROTO_OK;
+  }
+  return PROTO_ERROR_WRONG_PARAM;
+}
 
 byte getEEPROM(byte data[]){
   return EEPROM.read(data[PARAM]);
@@ -92,51 +167,6 @@ byte getEEPROM(byte data[]){
 byte setEEPROM(byte data[]){
   EEPROM.update(data[PARAM], data[VALUE]);
   return EEPROM.read(data[PARAM]);
-}
-
-byte (*proto[PROTO_MAX_COMMANDS])(byte data[]) = {setPWMValue, setPWMFrequency, getEEPROM, setEEPROM};
-
-
-void setup() {
-
-  pinMode(13, OUTPUT); // board Led
-
-  for(int i=0; i<pwmPinsNumber; i++){
-    pinMode(pwmPins[i], OUTPUT);
-    analogWrite(pwmPins[i], 0);
-  }
-    
-  Serial.begin(57600);
-  Serial.setTimeout(100);
-}
-
-void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
-}
-
-
-void serialEvent() {
-
-  byte buf[PROTO_RX_BUFFER_SIZE] = {0xff, 0xff ,0xff, 0xff}; // manual initialization
-        
-  if(Serial.readBytes(buf, PROTO_RX_BUFFER_SIZE) == PROTO_RX_BUFFER_SIZE){
-    if(checkXOR(buf)){
-      if(buf[COMMAND] < PROTO_MAX_COMMANDS){
-        Serial.write((*proto[buf[COMMAND]])(buf));
-      }else{
-        Serial.write(PROTO_ERROR_BAD_COMMAND);
-      }
-    }else{
-      Serial.write(PROTO_ERROR_XOR);
-    }
-  }else{
-    Serial.write(PROTO_ERROR_RX_FAULT);
-  }
-
-  while (Serial.available() > 0){Serial.read();} // clear serial buffer
 }
 
 bool checkXOR(byte data[]){
@@ -149,6 +179,10 @@ bool checkXOR(byte data[]){
   return (data[XOR] == checksum);  
 }
 
+void blinkBoardLed(){
+  pin13State =! pin13State;
+  pin13State ? digitalWrite(BOARD_LED_PIN, HIGH) : digitalWrite(BOARD_LED_PIN, LOW);
+}
 
 
-
+//
